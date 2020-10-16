08ff5672390ad97e877074d6777d929301f372a2
Mozdolevskyi Maksym <72501713+MozdolevskyiMaksym@users.noreply.github.com>
2020-10-16 12:13:03 +0300

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import Publisher from './helpers/publisher.js';
 // import { TemplateController } from './components/template/template-controller.js';
-import ControllerCarsGallery from './components/cars-gallery/controller-cars-gallery.js';
+import { ControllerCarsGallery } from './components/cars-gallery/controller-cars-gallery.js';
 import './css/main.scss';
 
 const publisher = new Publisher();
